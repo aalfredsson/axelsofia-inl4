f1cabdb89fd4e18e48e8f2a6dd2fb6e61d68b681
Axel Alfredsson <aalfredsson94@gmail.com>
2018-07-17 12:36:07 +0200

    Testing server up and running

--- index.php
+++ index.php
@@ -20,8 +20,8 @@ define('LARAVEL_START', microtime(true));
 | loading any of our classes later on. It feels great to relax.
 |
 */
-$app = require_once __DIR__.'/bootstrap/app.php';
-require __DIR__.'/vendor/autoload.php';
+
+require '/vendor/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ require __DIR__.'/vendor/autoload.php';
 |
 */
 
-
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
